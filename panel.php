49adabb4df95164a2845c4b86e99468962fd216b
Joaquingse <joaquingse@gmail.com>
2023-10-06 08:22:13 +0100

    testing new problems after login succesfully

--- panel.php
+++ panel.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>AdminLTE 3 | Dashboard</title>
+  <title>PHP practice</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback" />
@@ -33,18 +33,16 @@
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-<?php
-session_start();
+ <?php
+/* session_start();
 session_regenerate_id();
 
 if (!isset($_SESSION['username'])) {
   header('Location: index.php');
   exit();
 }
-
-
-
-?>
+ */
+?> 
 
 <body class="hold-transition sidebar-mini layout-fixed">
   <?php
