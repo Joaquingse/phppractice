414393818eda92915856f53e9ab83d3a8de4d097
Joaquín Gázquez <93706232+Joaquingse@users.noreply.github.com>
2023-10-23 09:28:39 +0100

    Update panel.php

--- panel.php
+++ panel.php
@@ -34,13 +34,13 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <?php
-  session_start();
+  /* session_start();
   session_regenerate_id();
 
   if (!isset($_SESSION['username'])) {
     header('Location: index.php');
     exit();
-  } 
+  } */ 
 
 ?>
 
