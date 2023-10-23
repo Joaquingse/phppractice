44398cc96c8abf448212aa51d70cbf6ab4dc3bbe
Joaquín Gázquez <93706232+Joaquingse@users.noreply.github.com>
2023-10-23 09:26:17 +0100

    Update panel.php

--- panel.php
+++ panel.php
@@ -33,16 +33,16 @@
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-<!--  <?php
-session_start();
-session_regenerate_id();
+<?php
+  session_start();
+  session_regenerate_id();
 
-if (!isset($_SESSION['username'])) {
-  header('Location: index.php');
-  exit();
-} 
+  if (!isset($_SESSION['username'])) {
+    header('Location: index.php');
+    exit();
+  } 
 
-?>  -->
+?>
 
 <body class="hold-transition sidebar-mini layout-fixed">
   <?php
